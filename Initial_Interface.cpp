20459c8e06150c22b0990ecb2cf03731fd52c97d
Reminiscent <161539770+Reminicent@users.noreply.github.com>
2024-04-19 23:48:17 +0700

    Add files via upload

--- Initial_Interface.cpp
+++ Initial_Interface.cpp
@@ -5,37 +5,58 @@
 #include <SDL_mixer.h>
 #include <bits/stdc++.h>
 
-
-MainMenu::MainMenu(SDL_Renderer* renderer) : renderer(renderer) {
+MainMenu::MainMenu(){}
+void MainMenu::Create_MainMenu(SDL_Renderer* renderer){
     SDL_Surface* play_surface = IMG_Load("play_button.png");
-    SDL_Surface* instructions_surface = IMG_Load("instructions_button.png");
-    SDL_Surface* quit_surface = IMG_Load("quit_button.png");
-
     playTexture = SDL_CreateTextureFromSurface(renderer, play_surface);
     SDL_FreeSurface(play_surface);
+    //play_surface=nullptr;
+
+    SDL_Surface* instructions_surface = IMG_Load("instructions_button.png");
     instructionsTexture = SDL_CreateTextureFromSurface(renderer, instructions_surface);
     SDL_FreeSurface(instructions_surface);
+    //instructions_surface = NULL;
+
+    SDL_Surface* quit_surface = IMG_Load("quit_button.png");
     quitTexture = SDL_CreateTextureFromSurface(renderer, quit_surface);
     SDL_FreeSurface(quit_surface);
 
+    SDL_Surface* credit_surface= IMG_Load("credit_button.png");
+    creditTexture = SDL_CreateTextureFromSurface(renderer, credit_surface);
+    SDL_FreeSurface(credit_surface);
+
+    //credit_surface = IMG_Load("highscore_button.png");
+    //creditTexture[2] = SDL_CreateTextureFromSurface(renderer, credit_surface);
+    //SDL_FreeSurface(credit_surface);
+
 }
 
-MainMenu::~MainMenu() {
+void MainMenu::Destroy_MainMenu() {
     SDL_DestroyTexture(playTexture);
     SDL_DestroyTexture(instructionsTexture);
     SDL_DestroyTexture(quitTexture);
+    SDL_DestroyTexture(creditTexture);
+    //SDL_DestroyTexture(creditTexture[2]);
+    //SDL_DestroyTexture(highscoreTexture);
 }
 
 
 void MainMenu::render(SDL_Renderer* renderer) {
 
     playButtonRect = { 700, 400, 200, 100 };
+
     instructionsButtonRect = { 700, 550, 200, 100 };
+    creditButtonRect = { 450, 550, 200, 100 };
+    highscoreButtonRect = { 950, 550, 200, 100 };
+
     quitButtonRect = { 700, 700, 200, 100 };
 
     SDL_RenderCopy(renderer, playTexture, NULL, &playButtonRect);
     SDL_RenderCopy(renderer, instructionsTexture, NULL, &instructionsButtonRect);
     SDL_RenderCopy(renderer, quitTexture, NULL, &quitButtonRect);
+    SDL_RenderCopy(renderer, creditTexture, NULL, &creditButtonRect);
+    //SDL_RenderCopy(renderer, creditTexture[2], NULL, &highscoreButtonRect);
+    //SDL_RenderCopy(renderer, highscoreTexture, NULL, &highscoreButtonRect);
 
 }
 
@@ -47,16 +68,29 @@ bool MainMenu::handleEvent(SDL_Event& e) {
         if (SDL_PointInRect(&clickPoint, &playButtonRect)) {
             return true;
         } else if (SDL_PointInRect(&clickPoint, &instructionsButtonRect)) {
-            Mix_Music* backgroundmusic1;    bool instruction_musicPlayed ;    Mix_Music* instruction_music;
-            //bool instruction_musicPlayed;
+            bool instruction_musicPlayed ;    Mix_Music* instruction_music;
             InstructionsMusic instructionsmusic;
             instruction_music = Mix_LoadMUS("instructions_music.mp3");
             if (!instruction_musicPlayed) {
-                      instructionsmusic.stop(backgroundmusic1);
                       instructionsmusic.play(instruction_music);
                       instruction_musicPlayed = true;
             }
         } else if (SDL_PointInRect(&clickPoint, &quitButtonRect)) {SDL_Quit();    return 0;
+        } else if (SDL_PointInRect(&clickPoint, &creditButtonRect)){
+            bool credit_musicPlayed = false;    Mix_Music* credit_music ;
+            credit_music = Mix_LoadMUS("credit_music.flac");
+            if (!credit_musicPlayed) {
+                      Mix_PlayMusic(credit_music,-1);
+                      credit_musicPlayed = true;
+            }
+        }else if (SDL_PointInRect(&clickPoint, &highscoreButtonRect)){
+            bool highscore_musicPlayed = false;    Mix_Music* highscore_music;
+            highscore_music = Mix_LoadMUS("highscore_music.mp3");
+            if (!highscore_musicPlayed) {
+                      Mix_HaltMusic();
+                      Mix_PlayMusic(highscore_music,-1);
+                      highscore_musicPlayed = true;
+            }
         }
     }
     return false;
--- Initial_Interface.h
+++ Initial_Interface.h
@@ -6,18 +6,28 @@
 
 class MainMenu {
 public:
-    MainMenu(SDL_Renderer* renderer);
-    ~MainMenu();
+    MainMenu();
+    void Destroy_MainMenu();
+
+    void Create_MainMenu(SDL_Renderer*renderer);
     void render(SDL_Renderer* renderer);
     bool handleEvent(SDL_Event& e);
 private:
     SDL_Renderer* renderer;
+
     SDL_Texture* playTexture;
     SDL_Texture* instructionsTexture;
     SDL_Texture* quitTexture;
+    SDL_Texture* creditTexture;
+    //SDL_Texture* highscoreTexture;
+
     SDL_Rect playButtonRect;
     SDL_Rect instructionsButtonRect;
     SDL_Rect quitButtonRect;
+    SDL_Rect creditButtonRect;
+    SDL_Rect highscoreButtonRect;
+
+
 };
 
 #endif // INITIAL_INTERFACE_H_INCLUDED
